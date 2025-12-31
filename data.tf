dd587f4b3b394220f2dbb0c073556f9a814486bd
siva2j22 <siva2j22@gmail.com>
2025-12-31 11:02:03 +0530

    files changed

--- data.tf
+++ data.tf
@@ -29,6 +29,10 @@ data "aws_ssm_parameter" "vpc_id" {
 
 }
 
+data "aws_ssm_parameter" "private_subnet_ids" {
+  name = "/${var.project_name}/${var.environment}/private_subnet_ids"
+}
+
 
 
 data "aws_ssm_parameter" "backend_alb_listener_arn" {
