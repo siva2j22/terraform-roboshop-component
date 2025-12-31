a7f70118a448b7323aa3c36939229f9b6a6242e3
siva2j22 <siva2j22@gmail.com>
2025-12-31 13:13:58 +0530

    update

--- main.tf
+++ main.tf
@@ -35,7 +35,6 @@ resource "terraform_data" "main" {
   provisioner "remote-exec" {
     inline = [
         "chmod +x /tmp/bootstrap.sh",
-        
         "sudo sh /tmp/bootstrap.sh ${var.component} ${var.environment}"
     ]
   }
