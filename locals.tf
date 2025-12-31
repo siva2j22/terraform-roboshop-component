0267c6737eaf95676cc2487d2d18f89c520c9c75
siva2j22 <siva2j22@gmail.com>
2025-12-31 12:17:14 +0530

    file change

--- locals.tf
+++ locals.tf
@@ -2,9 +2,9 @@ locals {
   common_name_suffix = "${var.project_name}-${var.environment}" # roboshop-dev
   private_subnet_id = split("," , data.aws_ssm_parameter.private_subnet_ids.value)[0]
   private_subnet_ids = split("," , data.aws_ssm_parameter.private_subnet_ids.value)
-  vpc_id = data.aws_ssm_parameter.vpc_id
+  vpc_id = data.aws_ssm_parameter.vpc_id.value
   ami_id = data.aws_ami.joindevops.id
-  sg_id = data.aws_ssm_parameter.sg_id
+  sg_id = data.aws_ssm_parameter.sg_id.value
   tg_port = "${var.component}" == "frontend" ? 80 : 8080
   health_check_path = "${var.component}" == "frontend" ?"/" : "/health"
   backend_alb_listener_arn = data.aws_ssm_parameter.backend_alb_listener_arn
